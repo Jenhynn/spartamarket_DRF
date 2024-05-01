64c2a7c33b960d49a301b40c721525f08dfa27f8
Jenhynn <159970972+Jenhynn@users.noreply.github.com>
2024-05-01 19:49:21 +0900

    상품 수정, 상품 삭제 기능 구현

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = "products"
 urlpatterns = [
     path("", views.product_list, name="product_list"),
-    path("<int:id>/", views.product_detail) 
+    path("<int:id>/", views.product_detail), # 상품 상세, 수정, 삭제
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -22,8 +22,21 @@ def product_list(request):
         # return Response(serializer.errors, status=400) # valid 하지 않으면 # raise_exception = True와 같음
 
 
-@api_view(["GET"])
+@api_view(["GET", "PUT", "DELETE"])
 def product_detail(request, id):
-    product = Product.objects.get(id=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+    if request.method == "GET":
+        product = get_object_or_404(Product, id=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+        
+    elif request.method == 'PUT':
+        product = get_object_or_404(Product, id=id) # 기존의 product를 가져와서
+        serializer = ProductSerializer(product, data=request.data, partial=True) # 그 자리에 data를 넣음 # partial을 넣으면 일부 필드만도 수정 가능
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+    
+    elif request.method == "DELETE":
+        product = get_object_or_404(Product, id=id)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
