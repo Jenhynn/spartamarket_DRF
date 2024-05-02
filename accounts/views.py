e28c68b4e0da902bb1681e5de90a629d4b32d92f
Jenhynn <159970972+Jenhynn@users.noreply.github.com>
2024-05-02 22:45:01 +0900

    회원가입 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,38 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
-from .serializers import SignupSerializer
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 
 
-class SignupAPIView(generics.CreateAPIView):
-    User = get_user_model()
-    queryset = User.objects.all()
-    serializer_class = SignupSerializer
+class SignupAPIView(APIView):
+
+    def post(self, request):
+        data = request.data
+        # username, email이 필수여야 함.
+        username = data.get("username")
+        email = data.get("email")
+        if not username or not email:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        if username == get_user_model().objects.filter(username=username).exists():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        if email == get_user_model().objects.filter(email=email).exists():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        user = get_user_model().objects.create_user(
+            username = username,
+            email = email,
+            password = data.get("password"),
+            nickname = data.get("nickname"),
+            birthdate = data.get("birthdate"),
+        )
+        return Response(
+            {
+                "username":user.username,
+                "message": "회원가입이 완료되었습니다."
+            },
+            status=status.HTTP_201_CREATED)
     
 
 # class ProfileAPIView():
