27cc83469969c74cadf89c0c2845f2c324153b84
Jenhynn <159970972+Jenhynn@users.noreply.github.com>
2024-05-02 23:28:04 +0900

    Product 모델에 image 추가

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     price = models.PositiveIntegerField()
     content = models.TextField()
+    image = models.ImageField(blank=True) # 빈 채로 저장 가능
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
