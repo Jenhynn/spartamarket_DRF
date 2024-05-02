6424af8d538710fd17d5292778fa1cbde3223551
Jenhynn <159970972+Jenhynn@users.noreply.github.com>
2024-05-02 15:02:52 +0900

    로그인 기능 - 커스텀 유저 모델 수정

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,8 @@
-from django.contrib.auth.models import User
 
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+
+from .models import User
 
 
 class SignupSerializer(serializers.ModelSerializer):
